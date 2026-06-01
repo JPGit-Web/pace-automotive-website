bd74837674331fe76444ff3083731d9b7ac60ae0
JPGit-Web <pizarrasjan@gmail.com>
2026-06-01 11:47:16 -0600

    Map tile fix

--- src/App.jsx
+++ src/App.jsx
@@ -270,7 +270,7 @@ export default function App() {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2511.943579792164!2d-113.99026632224191!3d50.980233471698156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53717a06f9d3ada1%3A0x2b41ece6116618d9!2s8240%2031%20St%20SE%20D%2C%20Calgary%2C%20AB%20T2C%201H9!5e0!3m2!1sen!2sca!4v1768685555052!5m2!1sen!2sca"
               width="100%"
               height="100%"
-              style={{ border: 0, minHeight: "360px", display: "block" }}
+              style={{ border: 0 }}
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
